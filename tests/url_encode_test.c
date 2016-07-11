b51ef320984fd4b9744b90d53fbe1869e8bdb8f8
Mohamed Boudra <boudra.moha@gmail.com>
2016-07-11 19:00:11 +0200

    url decode test

--- tests/url_encode_test.c
+++ tests/url_encode_test.c
@@ -10,7 +10,12 @@
 int main(int argc, char *argv[]) {
     const char *url = "=?/&%€Á";
     char out[200];
+
     gn_url_encode(url, out);
     assert(strcmp(out, "%3D%3F%2F%26%25%E2%82%AC%C3%81") == 0);
+
+    gn_url_decode("%3D%3F%2F%26%25%E2%82%AC%C3%81", out);
+    assert(strcmp(out, url) == 0);
+
     return 0;
 }
