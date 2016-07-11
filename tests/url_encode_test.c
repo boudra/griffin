56ae59d4723fdc0c469b7ea5ad334f4ab576474c
Mohamed Boudra <boudra.moha@gmail.com>
2016-07-11 18:58:35 +0200

    url encode test

---
+++ tests/url_encode_test.c
@@ -0,0 +1,16 @@
+#include "gn_map.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wctype.h>
+#include <locale.h>
+
+int main(int argc, char *argv[]) {
+    const char *url = "=?/&%€Á";
+    char out[200];
+    gn_url_encode(url, out);
+    assert(strcmp(out, "%3D%3F%2F%26%25%E2%82%AC%C3%81") == 0);
+    return 0;
+}
